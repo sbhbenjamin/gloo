ab8b7e3b772ce9b0bf754b1e1c1c73127bf17132
Benjamin <sbhbenjamin@gmail.com>
2021-07-10 20:33:00 +0800

    Remove history file

--- .history/frontend/cypress/integration/examples/gloo e2e tests/HomeScreen.test_20210623141716.js
+++
@@ -1,8 +0,0 @@
-///<reference types="cypress" />;
-
-describe("hello", () => {
-  it("test one", () => {
-    cy.visit("https://gloo-fixr.herokuapp.com");
-    cy.contains("Sign In").click().should("have.text", "Sign In");
-  });
-});
