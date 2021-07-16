c1cbc948f64d67117f6a774e2edb53208b941320
Benjamin <sbhbenjamin@gmail.com>
2021-07-16 18:07:15 +0800

    Remove cart from navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,11 @@
-import "./header.css"
-import React from "react"
-import { Route } from "react-router-dom"
-import { useDispatch, useSelector } from "react-redux"
-import { LinkContainer } from "react-router-bootstrap"
-import { Button, Navbar, Nav, Container, NavDropdown } from "react-bootstrap"
-import SearchBox from "./SearchBox"
-import { logout } from "../actions/userActions"
+import './header.css'
+import React from 'react'
+import { Route } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Button, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import SearchBox from './SearchBox'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
   const dispatch = useDispatch()
@@ -51,12 +51,12 @@ const Header = () => {
                 </LinkContainer>
               )}
 
-              <LinkContainer to='/cart' data-testid='navbar-cart'>
+              {/* <LinkContainer to='/cart' data-testid='navbar-cart'>
                 <Nav.Link>
                   <i className='fas fa-shopping-cart me-1'></i>
                   Cart
                 </Nav.Link>
-              </LinkContainer>
+              </LinkContainer> */}
 
               {userInfo ? (
                 <NavDropdown
