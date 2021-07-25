59556ee296c345b1a4f2a1d10bce16440a43407f
Chrus Chong <chrus1000@gmail.com>
2021-07-25 13:01:07 +0800

    Bug fixes for logout test (all e2e tests pass consistently)

--- frontend/cypress/integration/LogOut.test.js
+++ frontend/cypress/integration/LogOut.test.js
@@ -1,36 +1,32 @@
-import { HomeScreenTest } from "./HomeScreen.test.js"
-
 before(() => {
   cy.viewport(1280, 720)
-  cy.visit("https://gloo-dev.herokuapp.com")
+  cy.visit('https://gloo-dev.herokuapp.com')
 })
 
-describe("Logout feature works as expected", () => {
-  it("Sign in button can be clicked", () => {
-    cy.contains("Sign In").click()
+describe('Logout feature works as expected', () => {
+  it('Sign in button can be clicked', () => {
+    cy.contains('Sign In').click()
   })
 
-  it("Redirected to Sign In page", () => {
-    cy.url().should("include", "/login")
+  it('Redirected to Sign In page', () => {
+    cy.url().should('include', '/login')
   })
 
-  it("Able to login with correct email and password", () => {
+  it('Able to login with correct email and password', () => {
     cy.reload()
-    cy.get("[type='email']").type("john@example.com")
-    cy.get("[type='password']").type("123456")
-    cy.get("Button").contains("Sign In").click()
-    cy.contains("Invalid Email or Password").should("not.exist")
-    cy.contains("John Doe").should("exist").click()
-    cy.contains("Logout").should("exist")
+    cy.get("[type='email']").type('john@example.com')
+    cy.get("[type='password']").type('123456')
+    cy.get('Button').contains('Sign In').click()
+    cy.contains('Invalid Email or Password').should('not.exist')
+    cy.contains('John Doe').should('exist').click()
+    cy.contains('Logout').should('exist')
   })
 
-  it("Able to logout", () => {
-    cy.contains("Logout").should("exist").click()
+  it('Able to logout', () => {
+    cy.contains('Logout').should('exist').click()
   })
 
-  it("HomeScreen rerendered upon logout", () => {
-    cy.contains("John Doe").should("not.exist")
+  it('HomeScreen rerendered upon logout', () => {
+    cy.contains('John Doe').should('not.exist')
   })
-
-  HomeScreenTest()
 })
