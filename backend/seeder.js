6ad975ee3494b2455974c2d77ce6f8be67dbaece
sbhbenjamin <sbhbenjamin@gmail.com>
2021-07-25 12:37:57 +0800

    Update seed to delete all models on script run and add different users to products

--- backend/seeder.js
+++ backend/seeder.js
@@ -5,6 +5,10 @@ import users from './data/users.js'
 import products from './data/products.js'
 import User from './models/userModel.js'
 import Product from './models/productModel.js'
+import Cert from './models/certModel.js'
+import Conversation from './models/conversationModel.js'
+import Message from './models/messageModel.js'
+import Offer from './models/offerModel.js'
 import Order from './models/orderModel.js'
 import connectDB from './config/db.js'
 
@@ -17,15 +21,33 @@ const importData = async () => {
     await Order.deleteMany()
     await Product.deleteMany()
     await User.deleteMany()
+    await Cert.deleteMany()
+    await Conversation.deleteMany()
+    await Message.deleteMany()
+    await Offer.deleteMany()
 
     const createdUsers = await User.insertMany(users)
     const adminUser = createdUsers[0]._id
 
-    const sampleProducts = products.map((product) => {
-      return { ...product, user: adminUser }
-    })
+    const userZeroProducts = products
+      .filter((p, i) => i % 3 === 0)
+      .map((product) => {
+        return { ...product, user: createdUsers[0]._id }
+      })
+    const userOneProducts = products
+      .filter((p, i) => i % 3 === 1)
+      .map((product) => {
+        return { ...product, user: createdUsers[1]._id }
+      })
+    const userTwoProducts = products
+      .filter((p, i) => i % 3 === 2)
+      .map((product) => {
+        return { ...product, user: createdUsers[2]._id }
+      })
 
-    await Product.insertMany(sampleProducts)
+    await Product.insertMany(userZeroProducts)
+    await Product.insertMany(userOneProducts)
+    await Product.insertMany(userTwoProducts)
 
     console.log('Data Imported!'.green.inverse)
     process.exit()
